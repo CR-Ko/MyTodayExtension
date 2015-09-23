d7d5c784fe77ca761a8e9028d72b0fd41eac47e3
ko <ko@kos-macbook-pro.local>
2015-09-23 18:22:46 +0800

    Initial Commit

---
+++ MyExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyExtension
+//
+//  Created by ko on 2015/9/23.
+//  Copyright © 2015年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
